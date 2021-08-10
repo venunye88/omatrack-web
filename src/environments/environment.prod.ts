abf5c93b540f55c04fce94b86c0a94ef8a27aa3d
Daniel Fiadzeawu <dannyperez88@yahoo.com>
2021-08-10 09:05:58 +0000

    changed baseapi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // baseApi: 'api'
   // baseApi: "http://fuelmonitor.mytawo.com/api",
-  baseApi: "https://sb7q3vr7u0.execute-api.us-east-2.amazonaws.com/api"
+  baseApi: "https://yzqqjao4u0.execute-api.us-east-2.amazonaws.com/api"
 
 };
