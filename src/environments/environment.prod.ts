e58295472e2f7d4bc7fdbdb11e5851d27f9c4308
Daniel Fiadzeawu <dannyperez88@yahoo.com>
2021-08-13 09:52:49 +0000

    change baseapi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApi: 'api'
-  // baseApi: "https://yzqqjao4u0.execute-api.us-east-2.amazonaws.com/api"
+  // baseApi: 'api'
+  baseApi: "https://yzqqjao4u0.execute-api.us-east-2.amazonaws.com/api"
 
 };
